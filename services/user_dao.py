a01adc3fb46fb39cda198f865db611cfae4f8cd7
ayarkhan01 <ahmed@yarkhans.com>
2025-05-05 22:50:38 -0500

    made som equick changes

--- services/user_dao.py
+++ services/user_dao.py
@@ -1,12 +1,10 @@
 from db import get_session
 from models.users import User
-from werkzeug.security import generate_password_hash, check_password_hash
 
 def create_user(username, password):
     """Create a new user with hashed password"""
-    hashed_password = generate_password_hash(password)
     with get_session() as session:
-        user = User(username=username, password=hashed_password)
+        user = User(username=username, password=password)
         session.add(user)
         session.flush()
         session.refresh(user)
@@ -20,7 +18,7 @@ def password_match(username, password):
         user = session.query(User).filter_by(username=username).first()
         if not user:
             return False
-        return check_password_hash(user.password, password)
+        return (user.password, password)
 
 def get_user_id(username):
     """Get user ID by username"""
